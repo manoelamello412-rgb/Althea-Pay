a423edaa41f3f24005ca7937d1e180e4cc5c333d
manoelamello412-rgb <manoelamello412@gmail.com>
2026-09-04 19:40:36 -0300

    feat(mobile): integrate optimized dashboard header

--- components/dashboard-control.tsx
+++ components/dashboard-control.tsx
@@ -1,3 +1,13 @@
 'use client'
 
-export { default } from './dashboard-mobile-modern'
+import DashboardMobileModern from './dashboard-mobile-modern'
+import { MobileHeaderDashboard } from './mobile-header-dashboard'
+
+export default function DashboardControl() {
+  return (
+    <div className="bg-[#0B0B0D] [&>section>div>header]:hidden [&>section>div>header+div]:hidden">
+      <MobileHeaderDashboard />
+      <DashboardMobileModern />
+    </div>
+  )
+}
