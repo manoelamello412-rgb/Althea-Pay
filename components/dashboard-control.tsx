d3ed52fe19b45a2ec78e081bf524591e84bf64c1
manoelamello412-rgb <manoelamello412@gmail.com>
2026-09-05 23:17:46 -0300

    feat(dashboard): connect privacy-aware mobile dashboard

---
+++ components/dashboard-control.tsx
@@ -0,0 +1,3 @@
+'use client'
+import DashboardMobileModern from './dashboard-mobile-modern-v2'
+export default function DashboardControl(){return <div className="bg-[#0B0B0D]"><DashboardMobileModern/></div>}
