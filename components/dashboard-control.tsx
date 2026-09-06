ebadfd2b2ea8e992d87d51eb27b1e329eaa0ace6
manoelamello412-rgb <manoelamello412@gmail.com>
2026-09-05 22:34:41 -0300

    fix(ui): move dashboard header into shared shell

--- components/dashboard-control.tsx
+++ components/dashboard-control.tsx
@@ -1,13 +1,7 @@
 'use client'
 
 import DashboardMobileModern from './dashboard-mobile-modern'
-import { MobileHeaderDashboard } from './mobile-header-dashboard'
 
 export default function DashboardControl() {
-  return (
-    <div className="bg-[#0B0B0D] [&>section>div>header]:hidden [&>section>div>header+div]:hidden">
-      <MobileHeaderDashboard />
-      <DashboardMobileModern />
-    </div>
-  )
+  return <div className="bg-[#0B0B0D]"><DashboardMobileModern /></div>
 }
