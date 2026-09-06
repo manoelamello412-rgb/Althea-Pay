01ceac222a88e2c1cef22189326e3b7216f76ff4
manoelamello412-rgb <manoelamello412@gmail.com>
2026-09-05 23:17:38 -0300

    refactor(dashboard): enable privacy-aware mobile dashboard

--- components/dashboard-control.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import DashboardMobileModern from './dashboard-mobile-modern'
-
-export default function DashboardControl() {
-  return <div className="bg-[#0B0B0D]"><DashboardMobileModern /></div>
-}
