0f41be119ccf2eba4dc3c5dd462dba3cbc9d1802
manoelamello412-rgb <manoelamello412@gmail.com>
2026-09-08 23:28:15 -0300

    refactor(front): point dashboard control to canonical component

--- components/dashboard-control.tsx
+++ components/dashboard-control.tsx
@@ -1,3 +1,11 @@
 'use client'
-import DashboardMobileModern from './dashboard-mobile-modern-v2'
-export default function DashboardControl(){return <div className="bg-[#0B0B0D]"><DashboardMobileModern/></div>}
+
+import DashboardMobileModern from './dashboard-mobile-modern'
+
+export default function DashboardControl() {
+  return (
+    <div className="bg-[#0B0B0D]">
+      <DashboardMobileModern />
+    </div>
+  )
+}
