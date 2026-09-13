143e50c285755ea72f7f9f0d72fdc34fedeb003e
manoelamello412-rgb <manoelamello412@gmail.com>
2026-09-13 15:21:54 -0300

    fix: import usePathname from next/navigation

--- components/mobile-bottom-nav.tsx
+++ components/mobile-bottom-nav.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useEffect, usePathname, useState } from 'react'
+import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
 import { CircleDollarSign, GitBranch, LayoutGrid, MessageCircle, Sparkles } from 'lucide-react'
 
 const navItems = [
